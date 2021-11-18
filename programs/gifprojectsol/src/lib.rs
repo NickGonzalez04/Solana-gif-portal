f8370a47f420707586480a9120d3b177d475e91f
Nick Gonzalez <48919838+NickGonzalez04@users.noreply.github.com>
2021-11-17 23:21:06 -0500

    added in upvote and testing w/ mocha

--- programs/gifprojectsol/src/lib.rs
+++ programs/gifprojectsol/src/lib.rs
@@ -1,11 +1,11 @@
-// Simiular to import
 use anchor_lang::prelude::*;
+use anchor_lang::AccountsClose;
 
 // our program_id to run the program in Solana
-declare_id!("AV17oYk1DpdFkhioddypZhLWzxuTtSGAcJ2RbrETqFxP");
+declare_id!("H1XSeK4joK6pEF4sUd8oyBo53rhHpE7ARFpWN3uAr144");
+
 
 #[program]
-// defines our collection of functions
 pub mod gifprojectsol {
     use super::*;
     pub fn init_list(ctx: Context<InitList>, bump: u8) -> ProgramResult {
@@ -17,15 +17,22 @@ pub mod gifprojectsol {
         Ok(())
     }
 
+    pub fn delete_list(ctx: Context<InitList>) -> ProgramResult {
+        let user = &ctx.accounts.user;
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.close(user.to_account_info())?;
+        // let remove = base_account.gif_list
+        Ok(())
+    }
+
     // New fn instance that adds new gif
     pub fn add_new_gif(ctx: Context<AddNewGif>, gif_link: String ) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
         base_account.gif_list.push(GifItem {
-            gif_link: gif_link.to_string(),
+            gif_link,
             authority: *ctx.accounts.user.to_account_info().key,
             upvotes: 0,
         });
-
         Ok(())
     }
 
@@ -37,8 +44,8 @@ pub mod gifprojectsol {
             .iter_mut()
             .find(|gif| gif.gif_link == gif_link)
             .ok_or(Err::NoGifFound)?;
-            gif.upvotes +=1;
-            Ok(())
+        gif.upvotes += 1;
+        Ok(())
     }
 }
 
@@ -49,19 +56,34 @@ pub enum Err {
     #[msg("No Gif with that url")]
     NoGifFound,
 }
+
+// instru macro informing Anchor to deserialzie the instruction args so accounts can use the values
 #[derive(Accounts)]
-// instru macro informing ANchor to deserialzie the instruction args so accounts can use the values
 #[instruction(bump: u8)]
     pub struct InitList<'info> {
-        #[account(init, payer = user, seeds=[
-            b"ngGifme",
+        #[account(init, payer=user, seeds=[
+            b"ngGif2",
             user.to_account_info().key.as_ref()],
             bump=bump,
             space = 9000)]
         pub base_account: Account<'info, BaseAccount>,
         #[account(mut)]
         pub user: Signer<'info>,
-        pub system_program: Program <'info, System>,
+        pub system_program: Program<'info, System>,
+}
+
+// Delete a gif Item
+#[derive(Accounts)]
+pub struct DeleteList<'info> {
+    #[account(mut, seeds=[
+        b"ngGif2",
+        user.to_account_info().key.as_ref()], 
+        bump=base_account.bump
+    )]
+        pub base_account: Account<'info, BaseAccount>,
+        #[account(mut)]
+        pub user: Signer<'info>,
+        pub system_program: Program<'info, System>,
 }
 
 
@@ -69,10 +91,11 @@ pub enum Err {
  #[derive(Accounts)]
     pub struct AddNewGif<'info> {
         #[account(mut, seeds=[
-        b"ngGifme",
-        user.to_account_info().key.as_ref()  
+        b"ngGif2",
+        list_owner.to_account_info().key.as_ref()  
         ], bump=base_account.bump)]
         pub base_account: Account<'info, BaseAccount>,
+        pub list_owner: AccountInfo<'info>,
         pub user: Signer<'info>,
     }
 
@@ -80,16 +103,14 @@ pub enum Err {
 // Gif is the format behind saved gifs
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
 pub struct GifItem {
-    pub gif_link: String,
-    pub authority: Pubkey,
-    pub upvotes: u64,
+    gif_link: String,
+    authority: Pubkey,
+    upvotes: u64,
 }
 
 // Instructing Solana what we want to store to an account.
 #[account]
     pub struct BaseAccount {
-        pub total_gifs: u64,
         pub bump: u8,
-        // Vector (array) holding our gifs
         pub gif_list: Vec<GifItem>,
 }
--- tests/gif-list-test.js
+++ tests/gif-list-test.js
@@ -1,29 +1,28 @@
 const anchor = require('@project-serum/anchor');
-const { SystemProgram } = require('@solana/web3.js');
-const { expect} = require('chai');
+const { expect } = require('chai');
+const { SystemProgram } = anchor.web3;
 
+// test case for initalizing list, adding new gif to list
 describe('gif-project-sol', ()=>{
-
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
 
   const program = anchor.workspace.Gifprojectsol;
 
   const getBaseAcct = async () => {
-    let [baseAccount, bump] = await anchor.web3.PublicKey.findProgramAddress(
-      [Buffer.from("ngGifme"), provider.wallet.publicKey.toBytes()],
+    let [baseAccount, bump] = await web3.PublicKey.findProgramAddress(
+      [Buffer.from("ngGif2"), 
+      provider.wallet.publicKey.toBytes()],
       program.programId
     );
-
     return { baseAccount, bump };
   };
-
 
 
   const newGifList = async() => {
     let { baseAccount, bump } = await getBaseAcct();
 
-    const trxn = await program.rpc.initList(bump, {
+    await program.rpc.initList(bump, {
       accounts: {
         baseAccount,
         user: provider.wallet.publicKey,
@@ -39,24 +38,49 @@ describe('gif-project-sol', ()=>{
     it('Creates new gif list', async () => {
       console.log('🚀 new gif list coming ...');
       const { account } = await newGifList();
-      console.log('giflist count', account.gifList.length);
-      expect(account.gifList.length).eq(0);
-    })
+      console.log('giflist count', account.bump.toString());
+      console.log('Gif-accout', account.gifList.length);
+      // expect(account.gifList.length).eq(0);
+    });
+
 
     it('adds new gif to list', async ()=>{
-      let { baseAccount } = await getBaseAcct();
-      let account = await program.account.baseAccount.fetch(baseAccount);
-      console.log("GIT TOTAL--", account.gifList.length);
-  
-      // add in a new gift 
-      await program.rpc.addNewGif('https://media.giphy.com/media/NEvPzZ8bd1V4Y/giphy.gif', {
-          accounts: {
-            baseAccount,
-            user: provider.wallet.publicKey,
-          },
-          
-    })
-    account = await program.account.baseAccount.fetch(baseAccount);
-    expect(account.gifList.length).eq(1)
-  });
+        let { baseAccount } = await getBaseAcct();
+        let account = await program.account.baseAccount.fetch(baseAccount);
+        console.log("GIT TOTAL--", account.gifList.length);
+    
+        // add in a new gift 
+        await program.rpc.addNewGif("https://giphy.com/clips/grinchmovie-grinch-illumination-movie-Y0NhkIdyT3TVBDWLkG", {
+            accounts: {
+              baseAccount,
+              listOwner: provider.wallet.publicKey,
+              user: provider.wallet.publicKey,
+            },    
+      })
+
+      account = await program.account.baseAccount.fetch(baseAccount);
+      console.log('myGifs', account.gifList);
+      // expect(account.gifList.length).eq(1)
+    });
+
+
+    // it("deletes gif list", async () => {
+    //   let { baseAccount } = await getBaseAcct();
+    //   let account = await program.account.baseAccount.fetch(baseAccount);
+    //   console.log(provider);
+    //   // console.log(program)
+    //     console.log(account.gifList);
+    //   await program.rpc.deleteList({
+    //     accounts: {
+    //       baseAccount,
+    //       user: provider.wallet.publicKey,
+    //       systemProgram: SystemProgram.programId,
+    //     },
+    //   });
+    //  account = await program.account.baseAccount.fetch(baseAccount);
+    //   console.log('account', account.gifList);
+    //   // expect(account.gifList.length).eq(1);
+    // });
+
 });
+
